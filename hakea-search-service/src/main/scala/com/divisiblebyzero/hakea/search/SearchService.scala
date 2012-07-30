93de62bef9a013c2ca08af202a39808b10b85d62
Eric Czarny <eczarny@gmail.com>
2012-07-30 18:21:05 -0400

    Remove the assets from hakea-search-service in favor of the upcoming hakea-web project.

--- hakea-search-service/src/main/scala/com/divisiblebyzero/hakea/search/SearchService.scala
+++ hakea-search-service/src/main/scala/com/divisiblebyzero/hakea/search/SearchService.scala
@@ -3,12 +3,9 @@ package com.divisiblebyzero.hakea.search
 import com.divisiblebyzero.hakea.config.ServiceConfiguration
 import com.divisiblebyzero.hakea.search.resource.SearchResource
 import com.yammer.dropwizard.{ Logging, ScalaService }
-import com.yammer.dropwizard.bundles.AssetsBundle
 import com.yammer.dropwizard.config.Environment
 
 object SearchService extends ScalaService[ServiceConfiguration]("hakea-search-service") with Logging {
-  addBundle(new AssetsBundle)
-
   def initialize(configuration: ServiceConfiguration, environment: Environment) {
     val solrServer = configuration.hakea.solr.toSolrServer
 
